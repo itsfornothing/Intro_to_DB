5c16123b6464aae6c78593358d36e4c3a0cf8cce
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-01-29 14:27:59 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (orderdetailid INT AUTO_INCREMENT PRIMA
 title VARCHAR(130) NOT NULL,
 order_id INT,
 book_id INT,
-quantity double NOT NULL,
+quantity DOUBLE NOT NULL,
 FOREIGN KEY (book_id) REFERENCES Books(book_id),
 FOREIGN KEY (order_id) REFERENCES Orders(order_id));
 
