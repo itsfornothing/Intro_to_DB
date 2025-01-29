a159cee61a42ad688ed69dd0c1bd9555820bcb2a
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-01-29 14:46:18 +0300

    Add files via upload

---
+++ MySQLServer.py
@@ -0,0 +1,12 @@
+from mysql.connector import connect
+
+
+mydb = connect(host="127.0.0.1", user="root", passwd="Haha123@&$")
+
+my_cursor = mydb.cursor()
+
+my_cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+print("Database 'alx_book_store' created successfully!")
+
+my_cursor.close()
+mydb.close()
